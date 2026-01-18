a784073ff6a2d641d9bb50ec9b6a137c88be49fe
adrianspata <adrian.spata@hotmail.com>
2026-01-18 20:34:17 +0100

    feat: Refactor App component to include IntroAnimation and conditional rendering for Navigation and WorkStrip

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import ContactInfo from "./components/ContactInfo";
-import ImageGallery from "./components/ImageGallery";
+import IntroAnimation from "./components/IntroAnimation";
+import Navigation from "./components/Navigation";
+import WorkStrip from "./components/WorkStrip";
 import Description from "./components/Description";
 import ContactPage from "./pages/ContactPage"; 
 import SingleProjectPage from "./pages/SingleProjectPage";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [introComplete, setIntroComplete] = useState(false);
 
   return (
     <Router>
       <Routes>
         <Route path="/" element={
           <div className="app">
-            <Header />
-            <ContactInfo />
-            <ImageGallery />
-            <Description />
+            {!introComplete && (
+              <IntroAnimation onComplete={() => setIntroComplete(true)} />
+            )}
+            {introComplete && (
+              <>
+                <Navigation isVisible={introComplete} showLogo={true} />
+                <WorkStrip isVisible={introComplete} />
+                <Description />
+              </>
+            )}
           </div>
         } />
 
         <Route path="/contact" element={<ContactPage />} />
+
+        <Route path="/projects" element={<ContactPage />} />
 
         <Route path="/projects/:id" element={<SingleProjectPage />} />
 
